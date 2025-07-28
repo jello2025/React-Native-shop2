f2bdcaad8283f27e41a112328c1a81c3cfae9b2b
jello2025 <albarjas2025@gmail.com>
2025-07-28 20:05:16 +0300

    task 1

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,50 @@
-import { Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
+    <ScrollView
       style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        backgroundColor: "pink",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <View style={styles.container}>
+        <Text style={styles.title}>KDD milk shop</Text>
+        <Text style={styles.description}>
+          We sell KDD milk...obviously, its says it right there in the title,
+          dont y'all read?? gawd.
+        </Text>
+        <Image
+          source={require("../assets/images/kdd.jpg")}
+          style={styles.image}
+        />
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  image: {
+    height: 100,
+    width: 100,
+    borderRadius: 10,
+    borderColor: "white",
+    borderWidth: 5,
+  },
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 35,
+    paddingHorizontal: 40,
+    gap: 20,
+  },
+  title: {
+    fontSize: 45,
+    color: "white",
+    textAlign: "center",
+  },
+  description: {
+    fontSize: 20,
+    textAlign: "center",
+  },
+});
