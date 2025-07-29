54f6c0b520efc62a4e670cbbefcbcd587fd50121
jello2025 <albarjas2025@gmail.com>
2025-07-29 07:45:28 +0300

    task 2

---
+++ data/products.ts
@@ -0,0 +1,12 @@
+type Products = {
+  image: string;
+  name: string;
+  price: number;
+};
+
+const products: Products[] = [
+  { image: "../assets/images/chocomilk.png", name: "Choco milk", price: 0.25 },
+  { image: "", name: "Banana milk", price: 0.5 },
+];
+
+export default products;
